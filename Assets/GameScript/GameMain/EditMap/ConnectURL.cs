c11a6d7bcaa725170c5439250e107e72b03d4850
ShuoYue <judy25202@gmail.com>
2022-06-07 15:32:52 +0800

    6/7

---
+++ Assets/GameScript/GameMain/EditMap/ConnectURL.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectURL
+{
+    private string strURL = "";
+
+    public void fSetURL(string szURL)
+    {
+        strURL = szURL;
+    }
+
+    public void f_ConnectURL()
+    {
+        if (strURL.Equals("") || strURL == null) { return; }
+        Application.OpenURL(strURL);
+    }
+}
